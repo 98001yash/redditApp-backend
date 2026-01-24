4f031101cf0790db3df842fefd2c7aed02b623fc
98001yash <yashchauhan.gaya@gmail.com>
2026-01-24 15:50:15 +0530

    added the advices and exceptions

---
+++ user-service/src/main/java/com/redditApp/user_service/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.redditApp.user_service.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException() {
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
