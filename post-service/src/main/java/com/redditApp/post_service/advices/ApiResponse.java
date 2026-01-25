bb83ef16a3c9b212680b3d7f2f4c91d5502bc7c7
98001yash <yashchauhan.gaya@gmail.com>
2026-01-25 11:36:38 +0530

    aded the advices, auth and exceptions package in the post service

---
+++ post-service/src/main/java/com/redditApp/post_service/advices/ApiResponse.java
@@ -0,0 +1,29 @@
+package com.redditApp.post_service.advices;
+
+
+import com.redditApp.user_service.advices.ApiError;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ApiResponse<T> {
+
+    private LocalDateTime timeStamp;
+    private T data;
+    private ApiError error;
+
+    public ApiResponse(){
+        this.timeStamp = LocalDateTime.now();
+    }
+
+    public ApiResponse(T data){
+        this();
+        this.data = data;
+    }
+
+    public ApiResponse(ApiError error){
+        this();
+        this.error = error;
+    }
+}
