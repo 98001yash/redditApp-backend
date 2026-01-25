680b22e71845449958e0ca270e8db5c926e85b7f
98001yash <yashchauhan.gaya@gmail.com>
2026-01-25 10:02:04 +0530

    added the kafka-consumer in the user-service

---
+++ user-service/src/main/java/com/redditApp/user_service/events/UserSignedUpEvent.java
@@ -0,0 +1,22 @@
+package com.redditApp.user_service.events;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserSignedUpEvent {
+
+    private Long userId;
+    private String email;
+    private String displayName;
+    private String role;
+    private Instant createdAt;
+}
