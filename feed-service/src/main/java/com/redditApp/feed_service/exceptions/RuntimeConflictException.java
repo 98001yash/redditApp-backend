78f71e6862e0e89a39d459518296cdd25f4ec973
98001yash <yashchauhan.gaya@gmail.com>
2026-01-26 10:53:26 +0530

    added the advices, exceptions and modelMappr

---
+++ feed-service/src/main/java/com/redditApp/feed_service/exceptions/RuntimeConflictException.java
@@ -0,0 +1,11 @@
+package com.redditApp.feed_service.exceptions;
+
+public class RuntimeConflictException extends RuntimeException{
+    public RuntimeConflictException() {
+    }
+
+    public RuntimeConflictException(String message) {
+        super(message);
+    }
+}
+
