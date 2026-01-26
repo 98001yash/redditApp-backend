e56d8d6cbec5bb8139ae77b332d3b7ce30070b47
98001yash <yashchauhan.gaya@gmail.com>
2026-01-26 11:59:09 +0530

    added the kafka consumer and redis configuration in the feed-service

---
+++ feed-service/src/main/java/com/redditApp/feed_service/config/KafkaConsumerConfig.java
@@ -0,0 +1,49 @@
+package com.redditApp.feed_service.config;
+
+
+import com.redditApp.feed_service.event.PostCreatedEvent;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+
+    private static final String BOOTSTRAP_SERVERS = "localhost:9092";
+    private static final String GROUP_ID = "feed-service";
+
+    @Bean
+    public ConsumerFactory<String, PostCreatedEvent> consumerFactory() {
+
+        JsonDeserializer<PostCreatedEvent> valueDeserializer =
+                new JsonDeserializer<>(PostCreatedEvent.class, false); // false = ignore type info headers
+        valueDeserializer.addTrustedPackages("*"); // trust all packages
+
+        Map<String, Object> props = new HashMap<>();
+        props.put("bootstrap.servers", BOOTSTRAP_SERVERS);
+        props.put("group.id", GROUP_ID);
+        props.put("key.deserializer", StringDeserializer.class);
+        props.put("value.deserializer", valueDeserializer);
+
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), valueDeserializer);
+    }
+
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, PostCreatedEvent> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, PostCreatedEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
+
