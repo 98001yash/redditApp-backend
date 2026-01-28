45c74acac27238afd359255e26e542e423cc0787
98001yash <yashchauhan.gaya@gmail.com>
2026-01-28 08:04:18 +0530

    added the karma-microservice to the project?

---
+++ karma-service/src/test/java/com/redditApp/karma_service/KarmaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redditApp.karma_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarmaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
