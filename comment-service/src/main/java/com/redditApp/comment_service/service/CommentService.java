82b0d6cb24738dfe34ad84710b4cf9b6e6f65acf
98001yash <yashchauhan.gaya@gmail.com>
2026-01-28 18:59:12 +0530

    added the comment service interface

--- comment-service/src/main/java/com/redditApp/comment_service/service/CommentService.java
+++ comment-service/src/main/java/com/redditApp/comment_service/service/CommentService.java
@@ -1,4 +1,29 @@
 package com.redditApp.comment_service.service;
 
+import com.redditApp.comment_service.dtos.CommentRequest;
+import com.redditApp.comment_service.dtos.CommentResponse;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
 public interface CommentService {
+
+    // Add new comment
+    CommentResponse addComment(CommentRequest request);
+
+    // reply to comment
+    CommentResponse reply(Long parentId, CommentRequest request);
+
+    // delete
+    void delete(Long commentId);
+
+    // get comment of post
+    Page<CommentResponse> getPostComments(
+            Long postId,
+            int page,
+            int size
+    );
+
+    // get replies
+    List<CommentResponse> getReplies(Long parentId);
 }
