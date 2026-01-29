39e1b56d4094fc81cfd485771135cf9c7c59a048
98001yash <yashchauhan.gaya@gmail.com>
2026-01-29 08:14:03 +0530

    added the comment controller API

--- comment-service/src/main/java/com/redditApp/comment_service/controller/CommentController.java
+++ comment-service/src/main/java/com/redditApp/comment_service/controller/CommentController.java
@@ -5,11 +5,11 @@ import com.redditApp.comment_service.dtos.CommentRequest;
 import com.redditApp.comment_service.dtos.CommentResponse;
 import com.redditApp.comment_service.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/comment")
@@ -26,4 +26,42 @@ public class CommentController {
                 commentService.addComment(request)
         );
     }
+    // reply
+    @PostMapping("/{parentId}/reply")
+    public ResponseEntity<CommentResponse> reply(@PathVariable Long parentId,
+                                                 @RequestBody CommentRequest request){
+        return ResponseEntity.ok(commentService.reply(parentId, request));
+    }
+
+    @GetMapping("/post/{postId}")
+    public ResponseEntity<Page<CommentResponse>> getPostComments(
+            @PathVariable Long postId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        return ResponseEntity.ok(
+                commentService.getPostComments(postId, page, size)
+        );
+    }
+
+    // ---------------- GET REPLIES ----------------
+    @GetMapping("/{parentId}/replies")
+    public ResponseEntity<List<CommentResponse>> getReplies(
+            @PathVariable Long parentId) {
+
+        return ResponseEntity.ok(
+                commentService.getReplies(parentId)
+        );
+    }
+
+    // ---------------- DELETE ----------------
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable Long id) {
+
+        commentService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
